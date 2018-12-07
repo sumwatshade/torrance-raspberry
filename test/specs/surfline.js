b4956910a3d8c4a5133caa476be4c4a3614e35e1
Luke Shadler <lshadler13@gmail.com>
2018-12-07 13:58:58 -0800

    Organize url

--- test/specs/surfline.js
+++ test/specs/surfline.js
@@ -7,6 +7,14 @@ const SIGNIN_URL = 'https://www.surfline.com/sign-in?redirectUrl=https%3A%2F%2Fw
 const EMAIL = surflineConfig.email;
 const PASSWORD = surflineConfig.password;
 
+const urls = {
+    cardiff: 'https://www.surfline.com/surf-report/cardiff-reef-overview/5842041f4e65fad6a77088b1',
+    '15th-street': 'https://www.surfline.com/surf-report/15th-st-/5842041f4e65fad6a77088af',
+    swamis: 'https://www.surfline.com/surf-report/swami-s/5842041f4e65fad6a77088b4',
+    'north-san-diego': 'https://www.surfline.com/surf-forecasts/north-san-diego/58581a836630e24c44878fd7',
+    'south-san-diego': 'https://www.surfline.com/surf-forecasts/south-san-diego/58581a836630e24c4487900d'
+};
+
 describe('Sign in', () => {
     it('should have the right title - and sign in', () => {
     // Visit sign in page
@@ -20,8 +28,6 @@ describe('Sign in', () => {
 
         browser.pause(2000);
 
-        const { urls } = surflineConfig;
-
         const snapshotTime = new Date().getTime();
         Object.keys(urls).forEach((surfSpot) => {
             const surfSpotUrl = urls[surfSpot];
@@ -31,12 +37,6 @@ describe('Sign in', () => {
             console.log(browser.getTitle());
             browser.saveDocumentScreenshot(`./snapshots/${snapshotTime}/snapshot-${surfSpot}-${snapshotTime}.png`);
         });
-
-
-        // Get some data from surf report
-
-        // Save screenshot to file
-        // browser.saveDocumentScreenshot(`./snapshots/snapshot-cardiff-${new Date().getTime()}.png`);
 
         // Get HTML
         // http://webdriver.io/api/property/getHTML.html
